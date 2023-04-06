4138ebcba7edd1a7fa4331b765e73dd3a02143fa
publdaze <publdaze@naver.com>
2023-04-07 01:13:54 +0900

    feat : home에서 본인 페이지 접근 가능하도록 링크 추가
    #20

--- practice/src/pages/Home/Home.jsx
+++ practice/src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>스터디 실습</div>;
+  return (
+    <div>
+      <h1>리액트 스터디 실습</h1>
+      <Link to="publdaze">
+        <h3>publdaze</h3>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
