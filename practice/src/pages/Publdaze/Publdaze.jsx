199e67a1ecb4ab31d435da9cb7a98885a2dc6864
publdaze <publdaze@naver.com>
2023-04-07 02:00:13 +0900

    feat : Profile 컴포넌트 생성
    - Your first Component 실습
    
    #8

--- practice/src/pages/Publdaze/Publdaze.jsx
+++ practice/src/pages/Publdaze/Publdaze.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Profile from "./yourFirstComponent/Profile";
 
 const Publdaze = () => {
-  return <div>김은지 실습</div>;
+  return (
+    <div>
+      <h1>김은지 실습</h1>
+      <Profile />
+    </div>
+  );
 };
 
 export default Publdaze;
---
+++ practice/src/pages/Publdaze/yourFirstComponent/Profile.jsx
@@ -0,0 +1,3 @@
+export default function Profile() {
+  return <img src="https://i.imgur.com/lICfvbD.jpg" alt="Aklilu Lemma" />;
+}
